d35282587d4f271f41742d9d7a996f3e8c8b07dd
Babintcev <dmitry.babintsev1985@gmail.com>
2022-01-09 17:53:46 -0800

    CheckBoxes tests added

--- src/test/java/CheckBox.java
+++ src/test/java/CheckBox.java
@@ -3,24 +3,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class RadioButton {
+public class CheckBox {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/dmitr/Downloads/chromedriver_win32/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://formy-project.herokuapp.com/radiobutton");
+        driver.get("https://formy-project.herokuapp.com/checkbox");
 
-        WebElement radioButton1 = driver.findElement(By.id("radio-button-1"));
-        radioButton1.click();
+        WebElement checkBox1 = driver.findElement(By.id("checkbox-1"));
+        checkBox1.click();
         Thread.sleep(1000);
 
-        WebElement radioButton2 = driver.findElement(By.cssSelector("input[value='option2']"));
-        radioButton2.click();
+        WebElement checkBox2 = driver.findElement(By.cssSelector("input[value='checkbox-2']"));
+        checkBox2.click();
         Thread.sleep(1000);
 
-        WebElement radioButton3 = driver.findElement(By.xpath("/html/body/div/div[3]/input"));
-        radioButton3.click();
+        WebElement checkBox3 = driver.findElement(By.xpath("//*[@id=\"checkbox-3\"]"));
+        checkBox3.click();
         Thread.sleep(1000);
 
         driver.quit();
